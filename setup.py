66495bd91be73f2677abc0b28684b7defed623c2
Sandhya Kushwaha <Sanidhisuman25@gmail.com>
2024-09-13 19:37:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages , setup
+#from typing import List
+
+
+setup(
+
+name='Sensor',
+version='0.0.1',
+author='Sandhya Kushwaha',
+author_email="sanidhisuman25@gmail.com",
+packages= find_packages(),
+
+install_requires =["pymongo"]
+
+
+
+)
